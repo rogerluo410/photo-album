f196b297b8be76e45e329012feda73a1bd39b078
rogerluo410 <rogerluo410@gmail.com>
2015-04-13 17:38:17 +0800

    add validate

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessible :photo_name, :photo_addr, :photo_code, :photo_desc
   has_many :comments
-
+  validate :photo_name, presence:true, :on => :create
 
 
 end
